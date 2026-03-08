ece3dbea753e3c545b4e1272d8df1ced282999f7
CamelJR <aarcampion@gmail.com>
2026-03-07 23:49:46 -0800

    Minor fix

--- src/components/card-editor/CostEditor.tsx
+++ src/components/card-editor/CostEditor.tsx
@@ -36,7 +36,7 @@ export function CostEditor() {
     setImageField(`CostImage${slot}`, showCost ? 'CostBox.png' : '');
     setTextField(`Cost${slot}`, showCost ? cost : '');
     const costFontSize = cost.length >= 2 ? '10px' : '13px';
-    setStyleField(`Cost${slot}`, `{fontWeight:bold;fontSize:${costFontSize};top:-1px;left:-0.5px}`);
+    setStyleField(`Cost${slot}`, `{fontWeight:bold;fontSize:${costFontSize};top:-1px;left:-1px}`);
     setStyleField(`CostImage${slot}`, showCost ? '{border:1px solid rgba(0,0,0,1)}' : '');
   };
 
