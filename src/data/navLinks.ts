dc83824b3add54fc8796477cf2983d31c957352a
CamelJR <aarcampion@gmail.com>
2026-08-06 18:58:34 +0900

    Global components for header, footer, etc

---
+++ src/data/navLinks.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS = [
+  { label: 'Gallery', to: '/gallery' },
+  { label: 'Rulebook', to: '/rulebook' },
+  { label: 'Resources', to: '/resources' },
+  { label: 'About', to: '/about' },
+];
